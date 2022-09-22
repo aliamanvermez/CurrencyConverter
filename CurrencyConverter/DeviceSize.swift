da4e2d8d302225f1a61842e47cfba02ab5bbf4c2
Muhammet Ali Amanvermez <maliamanvermez@gmail.com>
2022-09-22 17:54:27 +0300

    Fixes

---
+++ CurrencyConverter/DeviceSize.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceSize.swift
+//  CurrencyConverter
+//
+//  Created by NeonApps on 22.09.2022.
+//
+
+import Foundation
+import UIKit
+
+
+    var screenWidth = CGFloat()
+    var screenHeight = CGFloat()
+
+    func setDefaultSize(view: UIView){
+        screenWidth = view.bounds.width
+        screenHeight = view.bounds.height
+    }
--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -1,19 +1,110 @@
-//
-//  ViewController.swift
-//  CurrencyConverter
-//
-//  Created by NeonApps on 29.08.2022.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var tryLabel =  UILabel()
+    var cucLabel = UILabel()
+    var usdLabel = UILabel()
+    var getRatesButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createUI()
+        view.backgroundColor = .white
     }
+    
+    
+    
+    func createUI() {
+        setDefaultSize(view: view)
+        tryLabel.frame = CGRect(x: 0.2 * screenWidth, y: 0.2 * screenHeight, width: 0.6 * screenWidth, height: 50)
+        cucLabel.frame = CGRect(x: 0.2 * screenWidth, y: 0.3 * screenHeight, width: 0.6 * screenWidth, height: 50)
+        usdLabel.frame = CGRect(x: 0.2 * screenWidth, y: 0.4 * screenHeight, width: 0.6 * screenWidth, height: 50)
+        getRatesButton.frame = CGRect(x: 0.3 * screenWidth, y: 0.5 * screenHeight, width: 0.4 * screenWidth, height: 50)
+        getRatesButton.setTitle("Get Rates", for: .normal)
+        getRatesButton.addTarget(self, action: #selector(getRatesButtonClicked), for: .touchUpInside)
+        getRatesButton.backgroundColor = .systemPink
+        view.addSubview(tryLabel)
+        view.addSubview(cucLabel)
+        view.addSubview(usdLabel)
+        view.addSubview(getRatesButton)
 
+    }
+    
+    
+    
+    
 
+    @objc func getRatesButtonClicked() {
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
+        
+      
+        let session = URLSession.shared
+
+        
+        //        Closure
+        let task = session.dataTask(with: url!) { data, _, error in
+            
+
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message:error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                
+                self.present(alert, animated: true, completion: nil)
+                
+            }
+            else {
+                //
+                if data != nil {
+                    
+                    do {
+                        
+                        let response = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? Dictionary <String,Any>
+                        //cast onemli String -->Any
+                        
+                        //mutable container icinde degistirilebilir json formati olusturuldu
+                        
+                        //asagidaki satir ASYNC calisir
+                        
+                        DispatchQueue.main.async {
+                            
+                            if let rates = response! ["rates"] as? [String : Any] {
+                                
+                                
+                                if let twd = rates["TWD"] as? Double {
+                                    self.tryLabel.text = "TWD : \(twd)"
+                                }
+                                
+                                if let usd = rates["USD"] as? Double {
+                                    
+                                    self.usdLabel.text = "USD : \(usd)"
+                                }
+                                
+                                if let cuc = rates["UYU"] as? Double {
+                                    self.cucLabel.text = "UYU : \(cuc)"
+                                
+                                }
+                                
+                               
+                            }
+                            
+                    
+                        }
+                        
+                    }catch {
+                        
+                    }
+                    
+                    
+                }
+            }
+        }
+            
+        task.resume()
+    }
+    
 }
 
